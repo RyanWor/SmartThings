24402c5196893e44a29f22a9f81b9d7736e8faf5
RyanWor <ryanworrell@gmail.com>
2016-06-10 16:29:50 -0500

    Update TimedDoorLock.groovy

--- TimedDoorLock.groovy
+++ TimedDoorLock.groovy
@@ -1,4 +1,4 @@
-definition(
+]definition(
     name: "Timed Door Lock",
     namespace: "jcholpuch",
     author: "Arnaud",
@@ -21,12 +21,17 @@ preferences{
 //    section("Automatically unlock the door when open...") {
 //        input "secondsLater", "number", title: "Delay (in seconds):", required: true
 //    }
-    section("Notifications") {
-        input "sendPush", "bool", required: false, // RW - Fixed broken push notification handling
-              title: "Send Push Notification?" // RW - Fixed broken push notification handling
-		input "phoneNumber", "phone", title: "Enter phone number to send text notification.", required: false
+//    section("Notifications") {
+//        input "sendPush", "bool", required: false, // RW - Fixed broken push notification handling
+//              title: "Send Push Notification?" // RW - Fixed broken push notification handling
+//		input "phoneNumber", "phone", title: "Enter phone number to send text notification.", required: false
+//    }
+    section("Send Notifications?") {
+        input("recipients", "contact", title: "Send notifications to") {
+            input "phone", "phone", title: "Warn with text message (optional)",
+                description: "Phone Number", required: false
+        }
     }
-
 }
 
 
@@ -51,17 +56,41 @@ def initialize(){
 def lockDoor(){
     log.debug "Locking the door."
     lock1.lock()
-    log.debug ( "Sending Push Notification..." )
-    if (sendPush) sendPush("${lock1} locked after ${minutesLater} minutes!") // RW - Fixed broken push notification handling
-    if ( phoneNumber != "0" ) sendSms( phoneNumber, "${lock1} locked after ${minutesLater} minutes!" )            
+//    log.debug ( "Sending Push Notification..." )
+//    if (sendPush) sendPush("${lock1} locked after ${minutesLater} minutes!") // RW - Fixed broken push notification handling
+//    if ( phoneNumber != "0" ) sendSms( phoneNumber, "${lock1} locked after ${minutesLater} minutes!" )            
+    log.debug "recipients configured: $recipients"
+
+    def message = "${lock1} locked after ${minutesLater} minutes!"
+    if (location.contactBookEnabled && recipients) {
+        log.debug "contact book enabled!"
+        sendNotificationToContacts(message, recipients)
+    } else {
+        log.debug "contact book not enabled"
+        if (phone) {
+            sendSms(phone, message)
+        }
+    }
 }
 
 def unlockDoor(){
     log.debug "Unlocking the door."
     lock1.unlock()
-    log.debug ( "Sending Push Notification..." ) 
-    if (sendPush) sendPush("${lock1} unlocked after ${contact} was opened for ${secondsLater} seconds!") // RW - Fixed broken push notification handling
-    if ( phoneNumber != "0" ) sendSms( phoneNumber, "${lock1} unlocked after ${contact} was opened for ${secondsLater} seconds!" )
+//    log.debug ( "Sending Push Notification..." ) 
+//    if (sendPush) sendPush("${lock1} unlocked after ${contact} was opened for ${secondsLater} seconds!") // RW - Fixed broken push notification handling
+//    if ( phoneNumber != "0" ) sendSms( phoneNumber, "${lock1} unlocked after ${contact} was opened for ${secondsLater} seconds!" )
+    log.debug "recipients configured: $recipients"
+
+    def message = "${lock1} unlocked after ${contact} was opened for ${secondsLater} seconds!"
+    if (location.contactBookEnabled && recipients) {
+        log.debug "contact book enabled!"
+        sendNotificationToContacts(message, recipients)
+    } else {
+        log.debug "contact book not enabled"
+        if (phone) {
+            sendSms(phone, message)
+        }
+    }
 }
 
 def doorHandler(evt){  // JCH - Simplify door handler for door without open/close switch
